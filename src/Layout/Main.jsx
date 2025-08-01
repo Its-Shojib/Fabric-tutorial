08eefd0a14bc7ed5eec83b88fc683502a6900494
Md Shojib Hossain <mdshojib922@gmail.com>
2025-08-02 02:39:02 +0600

    Added rectangle and circle shape

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,16 +2,16 @@ import { Outlet } from "react-router-dom";
 
 const Main = () => {
     return (
-        <div className="bg-gray-200 min-h-screen">
+        <div className="absolute inset-0">
             <div className="max-w-screen-4xl mx-auto">
                 <h1 className="text-4xl text-center">Fabric Tutorial</h1>
             </div>
             <div className="max-w-screen-2xl mx-auto my-20">
                 <Outlet></Outlet>
             </div>
-            <div className="max-w-screen-2xl mx-auto">
+            <div className="max-w-screen-2xl mx-auto absolute bottom-0 left-0 right-0 p-4">
                 <footer className="text-center">
-                    <p className="text-gray-500">©2025 Md Shojib Hossain. All Rights Reserved.</p>
+                    <p>©2025 Md Shojib Hossain | All Rights Reserved.</p>
                 </footer>
             </div>
         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,37 @@
-import { Canvas } from 'fabric';
+import { Canvas, Circle, Rect } from 'fabric';
 import { useEffect, useRef, useState } from 'react';
+import { FaCircle } from 'react-icons/fa';
+import { PiRectangleFill } from 'react-icons/pi';
 
 const Home = () => {
     const canvasRef = useRef(null);
     const [canvas, setCanvas] = useState(null);
+    console.log(canvas);
+
+    const addCircle = () => {
+        if (canvas) {
+            const circle = new Circle({
+                radius: 50,
+                fill: 'red',
+                left: 100,
+                top: 100,
+            });
+            canvas.add(circle);
+            canvas.renderAll();
+        }}
+
+        const addRectangle = () => {
+        if (canvas) {
+            const rectangle = new Rect({
+                fill: 'blue',
+                left: 50,
+                top: 100,
+                width: 100,
+                height: 50,
+            });
+            canvas.add(rectangle);
+            canvas.renderAll();
+        }}
 
     useEffect(() => {
         if(canvasRef.current){
@@ -23,9 +51,11 @@ const Home = () => {
     }, []);
 
     return (
-        <div className='flex flex-col items-center justify-center h-96  bg-gray-100'>
-            <h1 className="text-2xl font-bold mb-4">Fabric.js Canvas Example</h1>
-
+        <div className='flex flex-col items-center justify-center h-96'>
+            <div className='mb-4 my-16 w-32 flex justify-center items-center bg-gray-200 p-4 rounded-lg shadow-md gap-4'>
+                <PiRectangleFill className='text-3xl' onClick={addRectangle} />
+                <FaCircle className='text-3xl' onClick={addCircle} />
+            </div>
             <canvas  ref={canvasRef} className="border-2 z-20" />
         </div>
     );
